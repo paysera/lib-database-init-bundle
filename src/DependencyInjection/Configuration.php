3c6ca13203cd61fa4627fbe8603b89ec30826ee6
Tomas V <t.vaskevicius@paysera.com>
2021-01-22 13:46:01 +0200

    Handle root node

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('paysera_database_init');
         $rootNode = method_exists($treeBuilder, 'getRootNode') ? $treeBuilder->getRootNode() : $treeBuilder->root('paysera_database_init');
 
         $rootNode
